71ae4cd59908411c940374935e1ab4d3d4a1ab1f
jjhahaha <52387530+jjhahaha@users.noreply.github.com>
2022-01-16 23:51:18 +0800

    Online Game 選擇性別
    https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?a=223

---
+++ itsa/choose_gender.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a;
+    scanf("%s", &a);
+
+    if(a=='E')printf("ByeBye\n");
+    else if(a=='M')printf("Male\n");
+    else if(a=='F')printf("Female\n");
+
+}
